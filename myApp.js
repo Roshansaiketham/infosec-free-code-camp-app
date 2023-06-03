280c3b66991af5a1ec1b0f3c0a7cb047a033bc0d
Roshansaiketham <roshansaiketham@gmail.com>
2023-06-03 06:10:52 +0530

    Add noSniff for MIME protection

--- myApp.js
+++ myApp.js
@@ -4,11 +4,9 @@ const app = express();
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter())
+app.use(helmet.noSniff());
 
-app.use(express.urlencoded({extended:true}));
-app.post('/test',(req,res) => {
-  res.send('Input received:' + req.body.input);
-});
+
 
 
 
